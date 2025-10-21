9fee1d1b23d0f87c9f3776324d38a25b6dfa9de6
David Vasquez <dvasquez.422@gmail.com>
2025-10-20 21:06:22 -0600

    Created the footer for the app. Completed the UI.

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <div>
+      <footer className="rounded-lg shadow m-4 bg-blue-950">
+        <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+          <div className="text-center text-gray-500">
+            &copy; {new Date().getFullYear()}{" "}
+            <a href="https://davtek.io">Davtek</a> All Rights Reserved.
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
