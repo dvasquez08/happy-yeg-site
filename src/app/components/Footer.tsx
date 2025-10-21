d12ef6f77c75e5819821d0be7be983c44b239825
David Vasquez <dvasquez.422@gmail.com>
2025-10-21 13:48:15 -0600

    Changed the color of the background.

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div>
+    <div className="bg-zinc-50">
       <footer className="rounded-lg shadow m-4 bg-blue-950">
         <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
           <div className="text-center text-gray-500">
