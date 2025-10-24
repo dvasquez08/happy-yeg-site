80fd2e0eb11e3c0a30a39f9ec48290358f194fd4
David Vasquez <dvasquez.422@gmail.com>
2025-10-24 14:43:32 -0600

    Added privacy policy and disclaimer links

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
     <div className="bg-zinc-50">
@@ -7,6 +9,26 @@ const Footer = () => {
             &copy; {new Date().getFullYear()}{" "}
             <a href="https://davtek.io">Davtek</a> All Rights Reserved.
           </div>
+          <ul className="flex flex-wrap items-center justify-center mt-3 text-sm font-medium text-gray-400 sm:mt-0">
+            <li>
+              <Link href="/about" className="hover:underline me-4 md:me-6">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/privacy-policy"
+                className="hover:underline me-4 md:me-6"
+              >
+                Privacy Policy
+              </Link>
+            </li>
+            <li>
+              <Link href="/disclaimer" className="hover:underline">
+                Disclaimer
+              </Link>
+            </li>
+          </ul>
         </div>
       </footer>
     </div>
