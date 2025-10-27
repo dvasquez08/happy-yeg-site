7527522ba202b0713bd2c15312d96694a0edf605
David Vasquez <dvasquez.422@gmail.com>
2025-10-26 22:03:50 -0600

    Increased the size of the text in the intro paragraphs.

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -17,36 +17,27 @@ const Hero = () => {
               height={400}
             />
           </div>
-          <h1 className="mt-6 text-2xl font-semibold leading-tight">
+          <h1 className="mt-6 text-2xl font-semibold leading-tight text-zinc-200">
             Your Ultimate Guide to the Best Happy Hour Deals in Edmonton
           </h1>
           <p>Go out, have fun, and save money without the hassle.</p>
         </div>
       </div>
-      <div id="top" className="flex flex-col justify-center px-8 mt-8">
-        <p className="mb-4 ">
+      <div id="top" className="flex flex-col justify-center px-8 mt-24">
+        <p className="mb-4 text-xl leading-tight text-gray-800">
           These days, it&apos;s getting harder to spend money going out when
-          food prices keep getting higher, but it doesn&apos;t mean you
-          can&apos;t go out and celebrate without breaking the bank! Take
-          advantage of happy hours to save money while still being able to go
-          out.
+          food and drink prices keep getting higher, but it doesn&apos;t have to
+          mean you can&apos;t go out and celebrate without breaking the bank!
+          Take advantage of happy hours to save money while still being able to
+          go out.
         </p>
-        <p className="mb-4 ">
+        <p className="mb-4 text-xl leading-tight text-gray-800">
           Don&apos;t know what the happy hour specials are in the restaurants in
-          your area? Don&apos;t sweat, that&apos;s what this site is for!
-          Here&apos;s a list of happy hours throughout the city of Edmonton.
-          This list will grow as more research is done and more restaurants are
-          discovered. I will update as things change and will also be adding
-          daily specials soon as some of these places have some really good
-          ones. Have a look, and I hope you find a great place close to you!
-        </p>
-        <p className="">
-          I&apos;ll try to keep everything as up to date as I can, but if
-          there&apos;s something needs updating, or if you&apos;d like to let me
-          know about a restaurant that should be on the list let me know! Send
-          me a message through the contact form in the menu. If there&apos;s a
-          restaurant you want to look for, try a search in the search bar, also
-          in the menu.
+          your area? Happy YEG has you covered! Here&apos;s a list of happy
+          hours throughout the city of Edmonton. This list will grow as more
+          research is done and more restaurants are discovered. I will try my
+          best to keep things up to date as best as I can. Have a look, and I
+          hope you find a great place close to you!
         </p>
       </div>
     </div>
